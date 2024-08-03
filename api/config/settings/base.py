557fdc5ba9111915d322905fabed0724abccf1b5
Drosos Drosinakis <drosinakis.drosos1@gmail.com>
2024-08-04 01:29:49 +0300

    feat: add usermodel and tests

--- api/config/settings/base.py
+++ api/config/settings/base.py
@@ -31,7 +31,7 @@ THIRD_PARTY_APPS = [
     "djoser",
     "drf_spectacular",
 ]
-LOCAL_APPS = []
+LOCAL_APPS = ["core_apps.users"]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 MIDDLEWARE = [
@@ -126,6 +126,7 @@ MEDIA_ROOT = path.join(BASE_DIR, "mediafiles")
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+AUTH_USER_MODEL = "users.User"
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
