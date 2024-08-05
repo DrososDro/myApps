acc0c3823f9fb9bf62b443786f113109985ba3e4
Drosos Drosinakis <drosinakis.drosos1@gmail.com>
2024-08-05 14:16:38 +0300

    chore: finish the models of todos and profiles

--- api/core_apps/todos/admin.py
+++ api/core_apps/todos/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
+from core_apps.todos.models import Todos, TodosList
+
 # Register your models here.
+
+
+@admin.register(Todos)
+class TodosAdmin(admin.ModelAdmin):
+    list_display = ("name", "todos_list", "is_completed", "id")
+
+
+@admin.register(TodosList)
+class TodosAdminList(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "complete_until",
+        "is_completed",
+        "is_completed_in_time",
+        "id",
+    )
--- api/core_apps/todos/models.py
+++ api/core_apps/todos/models.py
@@ -1,11 +1,44 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 from core_apps.common.models import TimeStampedModel
+from core_apps.profiles.models import Profile
 
 # Create your models here.
+# TODO: need test for todos and  todos admin
+
+
+class TodosList(TimeStampedModel):
+    profile = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        related_name="profile",
+    )
+    title = models.CharField(max_length=50, verbose_name=_("Todo List Title"))
+    complete_until = models.DateTimeField(verbose_name=_("Todo Complete Time"))
+    is_completed_in_time = models.BooleanField(
+        default=False, verbose_name=_("Is Completed In Time")
+    )
+    is_completed = models.BooleanField(
+        default=False,
+        verbose_name=_("Is Completed"),
+    )
+
+    def __str__(self) -> str:
+        return str(self.title)
+
+    # TODO: make a method who calculates if the doto is completed in time
+    # TODO: make a method who calculates if the doto is completed at all
 
 
 class Todos(TimeStampedModel):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, verbose_name=_("Todo Name"))
     is_completed = models.BooleanField(default=False)
-    complete_until = models.DateTimeField()
+    todos_list = models.ForeignKey(
+        TodosList,
+        on_delete=models.CASCADE,
+        related_name="todos",
+    )
+
+    def __str__(self) -> str:
+        return self.name.__str__()
